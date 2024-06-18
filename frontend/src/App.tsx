1498e3c9ef925f4c83c7569351f2e4f16098f570
Yerovy Espitia <luyeesgar@gmail.com>
2024-06-18 14:14:32 -0500

    add tanstack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import {
   Card,
   CardContent,
@@ -6,18 +5,27 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/api'
+
+async function getTotalSpent() {
+  const res = await api.expenses['total-spent'].$get()
+  if (!res.ok) {
+    throw new Error('server error')
+  }
+  const data = await res.json()
+  return data
+}
 
 function App() {
-  const [totalSpent, setTotalSpent] = useState(0)
+  const { isPending, error, data } = useQuery({
+    queryKey: ['get-total-spent'],
+    queryFn: getTotalSpent,
+  })
 
-  useEffect(() => {
-    async function fetchTotal() {
-      const res = await fetch('api/expenses/total-spent')
-      const data = await res.json()
-      setTotalSpent(data.total)
-    }
-    fetchTotal()
-  }, [])
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occured: ' + error.message
 
   return (
     <>
@@ -26,7 +34,7 @@ function App() {
           <CardTitle>Total Spent</CardTitle>
           <CardDescription>The total amount you've spent</CardDescription>
         </CardHeader>
-        <CardContent>{totalSpent}</CardContent>
+        <CardContent>{isPending ? '...' : data.total}</CardContent>
       </Card>
     </>
   )
--- server/app.ts
+++ server/app.ts
@@ -7,9 +7,10 @@ const app = new Hono()
 
 app.get('*', logger())
 
-app.route('/api/expenses', expensesRoute)
+const apiRoutes = app.basePath('/api').route('/expenses', expensesRoute)
 
 app.get('*', serveStatic({ root: './frontend/dist' }))
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
+export type ApiRoutes = typeof apiRoutes
